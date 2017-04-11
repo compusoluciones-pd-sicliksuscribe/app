034614930285864b8cad6fd666ed0f87dcbbded3
Luis Villamil <lvillami@compusoluciones.com>
2017-04-11 10:50:53 -0500

    cambie conexiones del api y MAPI

--- app/appSettings.js
+++ app/appSettings.js
@@ -4,8 +4,8 @@ angular.module('marketplace')
   .run(function ($rootScope) {
     $rootScope.rsTitle = 'click suscribe | CompuSoluciones';
     $rootScope.rsVersion = '2.1.0';
-     $rootScope.API = 'http://10.3.102.52:8080/';
-     $rootScope.MAPI = 'http://10.3.102.52:8083/';
+     $rootScope.API = 'http://localhost:8080/';
+     $rootScope.MAPI = 'http://localhost:8083/';
      $rootScope.dominio = 'localhost';
     /* $rootScope.API = 'https://pruebas.compusoluciones.com/';
      $rootScope.MAPI = 'http://microsoft-api.us-east-1.elasticbeanstalk.com/';
