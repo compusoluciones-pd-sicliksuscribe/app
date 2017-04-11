1781112e9f234fa90215a127cfe2630fe93e5cbb
Luis Villamil <lvillami@compusoluciones.com>
2017-04-11 11:57:15 -0500

    cambios para pruebas

--- app/appSettings.js
+++ app/appSettings.js
@@ -1,6 +1,8 @@
 
 angular.module('marketplace')
 
+// cambio para que lo vea el gus
+
   .run(function ($rootScope) {
     $rootScope.rsTitle = 'click suscribe | CompuSoluciones';
     $rootScope.rsVersion = '2.1.0';
