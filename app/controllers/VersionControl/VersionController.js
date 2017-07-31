59de3ead06a0ba239cee6c097df811b9d63bf5c9
Gustavo Nuñez <gnunez@compusoluciones.com>
2017-07-31 17:06:09 -0500

    Se completó la página de mejoras

--- app/controllers/VersionControl/VersionController.js
+++ app/controllers/VersionControl/VersionController.js
@@ -11,10 +11,10 @@
     };
 
     $scope.obtenerVersiones = function () {
-      VersionFactory.getVersiones()
+        VersionFactory.getVersiones()
         .success(function(versiones){
           $scope.versiones = versiones.data;
-          $scope.obtenerDetalle();
+          obtenerDetalle();
         })
         .error(function (data, status, headers, config) {
             $scope.ShowToast('No pudimos traer las versiones.', 'danger');
@@ -23,21 +23,37 @@
         });
     };
 
-    $scope.obtenerDetalle = function () {
-      var IdVersion = $scope.versiones[0].Id;
+    var obtenerDetalle = function (Id) {
+      var IdVersion = Id || $scope.versiones[0].Id;
       VersionFactory.getVersionDetalle(IdVersion)
-        .success(function(versiones){
-          $scope.detalleVersion = versiones.data;
-        })
-        .error(function (data, status, headers, config) {
-            $scope.ShowToast('No pudimos traer el detalle de la versión.', 'danger');
-            $location.path('/uf/Carrito');
-            $log.log('data error: ' + data.error + ' status: ' + status + ' headers: ' + headers + ' config: ' + config);
-        });
+      .success(function(versiones){
+        $scope.detalleVersion = versiones.data;
+        SetTitulo();
+      })
+      .error(function (data, status, headers, config) {
+          $scope.ShowToast('No pudimos traer el detalle de la versión.', 'danger');
+          $location.path('/uf/Carrito');
+          $log.log('data error: ' + data.error + ' status: ' + status + ' headers: ' + headers + ' config: ' + config);
+      });
     };
 
     $scope.init();
 
+    $scope.GetDetalle = function (IdVersion, text) {
+      console.log(text);
+      if(!IdVersion){
+        IdVersion=4;
+      }
+      obtenerDetalle(IdVersion); 
+    };
+
+    var SetTitulo = function (){
+      var selectedIndex = document.getElementsByName("Versiones")[0].selectedIndex-1;
+      if(selectedIndex < 0) {
+        selectedIndex = 0;
+      }
+      $scope.Titulo = $scope.versiones[selectedIndex].Version;
+    };
   };
     
   VersionController.$inject = ['$scope', '$log', '$location', '$cookieStore', '$route', 'VersionFactory'];
