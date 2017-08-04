5edeb375d4eec98605dc64d563c5076d7f013df2
Luis Villamil <lvillami@compusoluciones.com>
2017-08-04 12:03:53 -0500

    Cambios para la navegación de pasos

--- app/controllers/Migracion/MigracionDetalleController.js
+++ app/controllers/Migracion/MigracionDetalleController.js
@@ -17,26 +17,60 @@
       { IdContexto: 2, Contexto: 'produccion' }
     ];
 
+    $scope.datosDeMigracion = {
+      NombreMigracion: '',
+      NombreCliente: '',
+      Dominio: '',
+      IdContexto: 1,
+      Contexto: 'sandox',
+      RelacionarMayorista: 0,
+      CrearAdministrador: 0,
+      ImportarDominio: 0,
+      OrdenarSuscripciones: 0,
+      CancelarSuscripciones: 0,
+      AsignarAsientos: 0
+    };
+
+    $scope.actualizarPasos = function () {
+      if ($scope.datosDeMigracion.RelacionarMayorista === 0) {
+        $scope.pasoActual = 1;
+        $scope.pasoSeleccionado = 1;
+        return;
+      }
+      if ($scope.datosDeMigracion.CrearAdministrador === 0) {
+        $scope.pasoActual = 2;
+        $scope.pasoSeleccionado = 2;
+        return;
+      }
+      if ($scope.datosDeMigracion.ImportarDominio === 0) {
+        $scope.pasoActual = 3;
+        $scope.pasoSeleccionado = 3;
+        return;
+      }
+      if ($scope.datosDeMigracion.OrdenarSuscripciones === 0) {
+        $scope.pasoActual = 4;
+        $scope.pasoSeleccionado = 4;
+        return;
+      }
+      if ($scope.datosDeMigracion.CancelarSuscripciones === 0) {
+        $scope.pasoActual = 5;
+        $scope.pasoSeleccionado = 5;
+        return;
+      }
+      if ($scope.datosDeMigracion.AsignarAsientos === 0) {
+        $scope.pasoActual = 6;
+        $scope.pasoSeleccionado = 6;
+        return;
+      }
+    };
+
     $scope.init = function () {
-      if ($scope.idMigracion !== 0) {
+      if ($scope.idMigracion !== '0') {
         MigracionFactory.getMigracion($scope.idMigracion)
           .then(function (response) {
             $scope.datosDeMigracion = response.data.data[0];
+            $scope.actualizarPasos();
           });
-      } else {
-        $scope.datosDeMigracion = {
-          NombreMigracion: '',
-          NombreCliente: '',
-          Dominio: '',
-          IdContexto: 1,
-          Contexto: 'sandox',
-          RelacionarMayorista: 0,
-          CrearAdministrador: 0,
-          ImportarDominio: 0,
-          OrdenarSuscripciones: 0,
-          CancelarSuscripciones: 0,
-          AsignarAsientos: 0
-        };
       }
     };
 
@@ -45,11 +79,20 @@
     $scope.crearMigracion = function () {
       console.log($scope.datosDeMigracion);
       var nuevaMigracion = {
-        NombreMigracion: $scope.datosDeMigracion,
+        NombreMigracion: $scope.datosDeMigracion.NombreMigracion,
         IdContexto: $scope.datosDeMigracion.IdContexto
       };
       MigracionFactory.postMigracion(nuevaMigracion)
-        .then(console.log);
+        .then(function (response) {
+          console.log(response, response.data.data.success);
+          if (response.data.success) {
+            $location.path('/migraciones/' + response.data.data.insertId);
+            return $scope.ShowToast(response.data.message, 'success');
+          }
+          $scope.ShowToast(response.data.message, 'danger');
+          $scope.pasoActual--;
+          $scope.pasoSeleccionado = $scope.pasoActual;
+        });
     };
 
     $scope.setSelected = function (index) {
