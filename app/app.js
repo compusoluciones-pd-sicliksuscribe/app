082a2a250ecebd93d1a38f927228414686a6ed46
Luis Villamil <lvillami@compusoluciones.com>
2017-10-02 15:42:10 -0500

    se agrego administrador cs 1 a control de versioens

--- app/app.js
+++ app/app.js
@@ -363,7 +363,7 @@
 
       .when('/Version', {
         controller: 'VersionController', templateUrl: 'app/views/VersionControl/VersionControl.html',
-        resolve: { 'check': function ($location, $cookieStore) { var Session = $cookieStore.get('Session'); if (!(Session.IdTipoAcceso === 2)) { $location.path('/404'); } } }
+        resolve: { 'check': function ($location, $cookieStore) { var Session = $cookieStore.get('Session'); if (!(Session.IdTipoAcceso === 2 && Session.IdTipoAcceso === 1)) { $location.path('/404'); } } }
       })
 
       /* .when('/:Subdominio', { controller: 'UsuariosLoginController', templateUrl: 'app/views/Usuarios/UsuariosLogin.html' }) */
