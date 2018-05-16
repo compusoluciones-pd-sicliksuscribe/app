c28ee92c61a61bb4f9da86761a91ccd8168e4c44
Jorge_Reynoso <jreynoso@compusoluciones.com>
2018-05-16 17:03:27 -0500

    se agrega la pestaña usuario, donde el mismo usuario puede cambiar su nombre, telefono etc

--- app/app.js
+++ app/app.js
@@ -23,7 +23,7 @@
         resolve: {
           'check': function ($location, $cookies) {
             var Session = $cookies.getObject('Session');
-            if (!(Session.IdTipoAcceso === 1 || Session.IdTipoAcceso === 2 || Session.IdTipoAcceso === 3 || Session.IdTipoAcceso === 4 || Session.IdTipoAcceso === 5 || Session.IdTipoAcceso === 6 || Session.IdTipoAcceso === 7)) { $location.path('/404');
+            if (!(Session.IdTipoAcceso === 1 || Session.IdTipoAcceso === 2 || Session.IdTipoAcceso === 3 || Session.IdTipoAcceso === 4 || Session.IdTipoAcceso === 5 || Session.IdTipoAcceso === 6 || Session.IdTipoAcceso === 7 || Session.IdTipoAcceso === 8)) { $location.path('/404');
             }
           }
         }
