a27d32525f2f23a22afe1e684b48913c06c5fa7d
Osvaldo Estrada [GDL] <osestrad@compusoluciones.com>
2018-07-11 17:40:19 -0500

    se quita el agregar productos adicionales a factura

--- app/appSettings.js
+++ app/appSettings.js
@@ -4,11 +4,11 @@ angular.module('marketplace')
   .run(function ($rootScope, $location, $anchorScroll, $routeParams) {
     $rootScope.rsTitle = 'click suscribe | CompuSoluciones';
     $rootScope.rsVersion = '2.1.1';
-    // $rootScope.API = 'http://localhost:8080/';
-    // $rootScope.MAPI = 'http://localhost:8083/';
-    // $rootScope.dominio = 'localhost';
-    $rootScope.API = 'https://pruebas.compusoluciones.com/';
-    $rootScope.MAPI = 'http://microsoft-api.us-east-1.elasticbeanstalk.com/';
-    $rootScope.dominio = 'clicksuscribe';
-    $rootScope.secureCookie = true;
+    $rootScope.API = 'http://localhost:8080/';
+    $rootScope.MAPI = 'http://localhost:8083/';
+    $rootScope.dominio = 'localhost';
+    // $rootScope.API = 'https://pruebas.compusoluciones.com/';
+    // $rootScope.MAPI = 'http://microsoft-api.us-east-1.elasticbeanstalk.com/';
+    // $rootScope.dominio = 'clicksuscribe';
+    $rootScope.secureCookie = false;
   });
