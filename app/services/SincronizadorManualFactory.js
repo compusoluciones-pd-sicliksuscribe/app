5c36056540534aa6a1f372be279e9e07b7df4d8b
Kathia Elizalde [GDL] <kelizald@compusoluciones.com>
2018-09-11 10:19:33 -0500

    Generar ruta que obtenga los datos de la BD para el llenado de la tabla del sincronizador manual

---
+++ app/services/SincronizadorManualFactory.js
@@ -0,0 +1,25 @@
+(function () {
+  var SincronizadorManualFactory = function ($http, $cookies, $rootScope) {
+    var factory = {};
+    var Session = {};
+
+    factory.refreshToken = function () {
+      Session = $cookies.getObject('Session');
+      if (!Session) { Session = { Token: 'no' }; }
+      $http.defaults.headers.common['token'] = Session.Token;
+    };
+
+    factory.refreshToken();
+
+    factory.getSincronizadorManual = function () {
+      factory.refreshToken();
+      return $http.get($rootScope.API + 'microsoft/manualsynchronizer');
+    };
+
+    return factory;
+  };
+
+  SincronizadorManualFactory.$inject = ['$http', '$cookies', '$rootScope'];
+
+  angular.module('marketplace').factory('SincronizadorManualFactory', SincronizadorManualFactory);
+}());
