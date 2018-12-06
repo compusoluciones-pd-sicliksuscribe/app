028f7f7c965ef50ab2a72871b1433e094a9f6c7e
OSvaldo <osvaldoestradac@gmail.com>
2018-12-06 13:42:23 -0600

    se integra respuesta de la ruta en api para mostrar el sitio embebido.

--- app/controllers/Vmware/MonitorDetalleWmwareController.js
+++ app/controllers/Vmware/MonitorDetalleWmwareController.js
@@ -1,16 +1,18 @@
 (function () {
-    var MonitorDetalleVmwareController = function ($scope, $log, $cookies, $location, EmpresasXEmpresasFactory, PedidoDetallesFactory, $uibModal, $filter, FabricantesFactory, PedidosFactory, EmpresasFactory, UsuariosFactory) {
+    var MonitorDetalleVmwareController = function ($scope, $sce, $cookies, $location, EmpresasXEmpresasFactory, PedidoDetallesFactory, $uibModal, $filter, FabricantesFactory, PedidosFactory, EmpresasFactory, UsuariosFactory) {
       $scope.SessionCookie = $cookies.getObject('Session');
+      
 
       const checkUser = function () {
         if($scope.SessionCookie.IdTipoAcceso !== 1 || $scope.SessionCookie.IdTipoAcceso !== 8) {
             FabricantesFactory.getUriVmwareDistributor()
             .success(function (Uri) {
-                if (Uri.success === 1) {
+                if (Uri) {
+                    $scope.url = $sce.trustAsResourceUrl(Uri);
                 }
             })
             .error(function (data, status, headers, config) {
-                $scope.ShowToast('Monitor simulado.', 'danger');
+                $scope.ShowToast('El distribuidor no cuenta con datos en Vmware', 'danger');
             });
         }
       }
@@ -23,7 +25,7 @@
       $scope.init();
     }
   
-    MonitorDetalleVmwareController.$inject = ['$scope', '$log', '$cookies', '$location', 'EmpresasXEmpresasFactory', 'PedidoDetallesFactory', '$uibModal', '$filter', 'FabricantesFactory', 'PedidosFactory', 'EmpresasFactory', 'UsuariosFactory'];
+    MonitorDetalleVmwareController.$inject = ['$scope', '$sce', '$cookies', '$location', 'EmpresasXEmpresasFactory', 'PedidoDetallesFactory', '$uibModal', '$filter', 'FabricantesFactory', 'PedidosFactory', 'EmpresasFactory', 'UsuariosFactory'];
   
     angular.module('marketplace').controller('MonitorDetalleVmwareController', MonitorDetalleVmwareController);
   }());
