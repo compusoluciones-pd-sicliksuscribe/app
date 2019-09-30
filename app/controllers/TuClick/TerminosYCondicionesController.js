ba3fe51e6d8e8c6d9ac6c0a3ec8f7c03b0e01945
Marco Hernandez <marc.hernandez.r@gmail.com>
2019-09-30 15:18:55 -0500

    Carga condicional según extensión de archivo tuclick

--- app/controllers/TuClick/TerminosYCondicionesController.js
+++ app/controllers/TuClick/TerminosYCondicionesController.js
@@ -1,9 +1,22 @@
 (function () {
     var TerminosYCondicionesController = function ($scope, $log, $cookies,  $sce) {
        $scope.currentDistribuidor = $cookies.getObject('currentDistribuidor');
-       $scope.terminosPDF = $sce.trustAsResourceUrl($scope.currentDistribuidor.TerminosYCondiciones);
+       let terminosPDF = $scope.currentDistribuidor.TerminosYCondiciones;
+       let terminos;
+
+       if (getFileExtension(terminosPDF) === 'pdf') {
+         terminos  = `<embed style="width:100%; height:700px;" src="${terminosPDF}" type="application/pdf" />`;
+       } else {
+         terminos  = `<img style="width:100%; height:700px;" src="${terminosPDF}"  frameborder="0"> </img>`;
+       }    
+    document.getElementById('terminos').innerHTML = terminos;
+    
     };
-  
+
+    function getFileExtension(filename) {
+      return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+    }
+
     TerminosYCondicionesController.$inject = ['$scope', '$log', '$cookies', '$sce'];
   
     angular.module('marketplace').controller('TerminosYCondicionesController', TerminosYCondicionesController);
