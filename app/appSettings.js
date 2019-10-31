bdb0b0bfda5db59525fdbccd0a139757b825185e
OSvaldo <osvaldoestradac@gmail.com>
2019-10-31 08:01:09 -0600

    se unen opciones de editar datos y cerrar sesión en una opcion en el menú, se crea la liga a siclik

--- app/appSettings.js
+++ app/appSettings.js
@@ -7,7 +7,10 @@ angular.module('marketplace')
     $rootScope.API = 'http://localhost:8080/';
     $rootScope.MAPI = 'http://localhost:8083/';
     $rootScope.dominio = 'localhost';
-    $rootScope.SICLICK_API = 'http://10.3.112.97:8998/'
+    $rootScope.SICLIK_API = 'http://10.3.112.97:8998/'
+    $rootScope.SICLIK_FRONT = 'http://10.3.112.113:3000/'
+    // $rootScope.SICLIK_FRONT = 'https://siclik.mx/'
+    // $rootScope.SICLIK_API = ''
     // $rootScope.API = 'https://pruebas.compusoluciones.com/';
     // $rootScope.MAPI = 'http://microsoft-api.us-east-1.elasticbeanstalk.com/';
     // $rootScope.dominio = 'clicksuscribe';
