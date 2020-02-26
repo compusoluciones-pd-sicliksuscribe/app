bf53e5f23f6e4316d6048ea8bb36b926bbb20183
Marco Hernandez [GDL] <mhernandez@compusoluciones.com>
2020-02-26 15:48:21 -0600

    Se cambia vista de ontacto solo para cadgrafics

--- app/controllers/TuClick/DatosContactoController.js
+++ app/controllers/TuClick/DatosContactoController.js
@@ -1,24 +1,24 @@
 (function () {
-    var DatosContactoController = function ($scope, $log, $cookies,  $sce) {
-       $scope.currentDistribuidor = $cookies.getObject('currentDistribuidor');
-       let UrlContacto = $scope.currentDistribuidor.UrlContacto;
-       let contact;
+  var DatosContactoController = function ($scope, $log, $cookies, $sce) {
+    $scope.currentDistribuidor = $cookies.getObject('currentDistribuidor');
+    let UrlContacto = $scope.currentDistribuidor.UrlContacto;
+    let contact;
 
-       if (getFileExtension(UrlContacto) === 'pdf') {
-          contact  = `<embed  style="width:100%; height:700px;" src="${UrlContacto}" type="application/pdf" />`;
-       } else {
-         contact  = `<img style="width:100%; height:700px;" src="${UrlContacto}"  frameborder="0"> </img>`;
-       }    
-    document.getElementById('contact').innerHTML = contact;
-    
-    };
-
-    function getFileExtension(filename) {
-      return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+    if ($scope.currentDistribuidor.Subdominio !== 'cadgrafics') {
+      if (getFileExtension(UrlContacto) === 'pdf') {
+        contact = `<embed  style="width:100%; height:700px;" src="${UrlContacto}" type="application/pdf" />`;
+      } else {
+        contact = `<img style="width:100%; height:700px;" src="${UrlContacto}"  frameborder="0"> </img>`;
+      }
+      document.getElementById('contact').innerHTML = contact;
     }
-  
-    DatosContactoController.$inject = ['$scope', '$log', '$cookies', '$sce'];
-  
-    angular.module('marketplace').controller('DatosContactoController', DatosContactoController);
-  }());
-  
+  };
+
+  function getFileExtension (filename) {
+    return filename.slice((filename.lastIndexOf('.') - 1 >>> 0) + 2);
+  }
+
+  DatosContactoController.$inject = ['$scope', '$log', '$cookies', '$sce'];
+
+  angular.module('marketplace').controller('DatosContactoController', DatosContactoController);
+}());
