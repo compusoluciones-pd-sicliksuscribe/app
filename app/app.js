9199a9f7de2c8322a21c56c2c543ad8874f422cf
Osvaldo Estrada [GDL] <osestrad@compusoluciones.com>
2020-07-08 13:51:45 -0500

    se crea ruta para asignar y eliminar descuento de promoción para azure.

--- app/app.js
+++ app/app.js
@@ -190,6 +190,16 @@
         }
       })
 
+      .when('/DescuentoPromoAzure/:IdEmpresa', {
+        controller: 'EmpresaAzurePromoController', templateUrl: 'app/views/Empresas/EmpresaAzurePromo.html',
+        resolve: {
+          'check': function ($location, $cookies) {
+            var Session = $cookies.getObject('Session');
+            if (!(Session.IdTipoAcceso === 1 || Session.IdTipoAcceso === 8)) { $location.path('/404'); }
+          }
+        }
+      })
+
       .when('/Monitor', {
         controller: 'MonitorReadController', templateUrl: 'app/views/PedidoDetalles/MonitorRead.html',
         resolve: {
